585d3f8137884b87672944330124bb3767dc1c1d
Jean-Michel Cheumeni <jean-michel@nibble.ai>
2021-11-18 15:07:30 +0000

    Add constant file

---
+++ day_1/src/constants.py
@@ -0,0 +1,21 @@
+# filename
+PRODUCTS = 'products.csv'
+TRANSACTIONS = 'transactions.csv'
+
+# file "transactions.csv" column
+DATE = 'date'
+PRODUCT_ID = 'product_id'
+STORE_ID = 'store_id'
+NB_SOLD_PIECES = 'nb_sold_pieces'
+
+# file "stores.csv" column
+CITY = 'city'
+SURFACE_AREA = 'surface_area'
+
+# file "product_id"
+PRODUCT_NAME = 'product_name'
+GROSS_PRICE = "gross_price"
+COLOR = 'color'
+SUPPLIER = 'supplier'
+
+# Windows
